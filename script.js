14cc6104262a189b8bfcda9b5e02759c52b95043
giuseppevignanello <peppe.vignanello@gmail.com>
2023-07-28 14:41:12 +0200

    add characters list

---
+++ script.js
@@ -0,0 +1,37 @@
+characters = [
+  {
+    name: "Homer Simpson",
+    image:
+      "https://35milimetros.es/wp-content/uploads/2016/02/homer-simpson.jpg",
+  },
+  {
+    name: "Micheal Scott",
+    image:
+      "https://www.lavanguardia.com/files/og_thumbnail/uploads/2020/06/29/5fa91c5e49c91.jpeg",
+  },
+  {
+    name: "Walter White",
+    image:
+      "https://vader.news/__export/1616459037431/sites/gadgets/img/2021/03/22/walter-white.jpg_1841249101.jpg",
+  },
+  {
+    name: "Goku",
+    image:
+      "https://35milimetros.es/wp-content/uploads/2016/02/homer-simpson.jpg",
+  },
+  {
+    name: "Tyrion Lannister",
+    image:
+      "https://fotografias.antena3.com/clipping/cmsimages02/2012/08/08/800B0E58-0268-4E5C-84F4-0CBA36E7D7EB/98.jpg?crop=1024,576,x0,y0&width=1900&height=1069&optimize=low&format=webply",
+  },
+  {
+    name: "Hermione Granger",
+    image:
+      "https://media.glamour.com/photos/58a72f5277d3274eb940938f/4:3/w_1987,h_1490,c_limit/hermione.jpg",
+  },
+  {
+    name: "Lisa Simpson",
+    image:
+      "https://7televalencia.com/wp-content/uploads/2019/04/lisasimpsonnok.jpg",
+  },
+];
